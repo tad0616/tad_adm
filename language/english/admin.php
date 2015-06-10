bfbbef3e1b0f1dbe77e02c00a6fcd36ad30cef18
Tad <tad0616@gmail.com>
2015-06-10 14:32:18 +0800

    調整語系

--- language/english/admin.php
+++ language/english/admin.php
@@ -1,5 +1,5 @@
 <?php
-include_once "../../tadtools/language/{$xoopsConfig['language']}/admin_common.php";
+include_once '../../tadtools/language/' . $xoopsConfig['language'] . '/admin_common.php';
 define('_TAD_NEED_TADTOOLS', "This module needs TadTools module. You can download TadTools from <a href='http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50' target='_blank'>Tad's web</a>.");
 
 //phpini.php
--- language/tchinese_utf8/admin.php
+++ language/tchinese_utf8/admin.php
@@ -1,5 +1,5 @@
 <?php
-include_once "../../tadtools/language/{$xoopsConfig['language']}/admin_common.php";
+include_once '../../tadtools/language/' . $xoopsConfig['language'] . '/admin_common.php';
 define('_TAD_NEED_TADTOOLS', ' 需要 tadtools 模組，可至<a href="http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50" target="_blank">Tad教材網</a>下載。');
 
 //phpini.php
