fda22e71cde3d0107819387ebe6560bf3d496750
mambax7 <mambax7@gmail.com>
2017-09-02 04:35:11 -0400

    __DIR__

--- xoops_version.php
+++ xoops_version.php
@@ -11,7 +11,7 @@ $modversion['help']        = 'page=help';
 $modversion['license']     = 'GNU GPL 2.0';
 $modversion['license_url'] = 'www.gnu.org/licenses/gpl-2.0.html/';
 $modversion['image']       = "images/logo_{$xoopsConfig['language']}.png";
-$modversion['dirname']     = basename(dirname(__FILE__));
+$modversion['dirname']     = basename(__DIR__);
 
 //---模組狀態資訊---//
 $modversion['release_date']        = '2017/06/16';
