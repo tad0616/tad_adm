defe7f732a03e26167e7c6e351e3c7d525929a57
Tad <tad0616@gmail.com>
2019-05-10 15:17:40 +0800

    更新安裝程式

--- include/onUpdate.php
+++ include/onUpdate.php
@@ -2,6 +2,10 @@
 
 use XoopsModules\Tad_adm\Update;
 
+if (!class_exists('XoopsModules\Tad_adm\Update')) {
+    include dirname(__DIR__) . '/preloads/autoloader.php';
+}
+
 function xoops_module_update_tad_adm()
 {
     global $xoopsDB;
