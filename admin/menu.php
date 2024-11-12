184e1255cffdf808738709bf38c42c279d7a62a2
tad0616 <tad0616@gmail.com>
2024-11-12 16:20:04 +0800

    簡化後台選單陣列，修改釋出日期

--- admin/menu.php
+++ admin/menu.php
@@ -1,50 +1,10 @@
 <?php
-$adminmenu = [];
-$icon_dir = '2.6' === mb_substr(XOOPS_VERSION, 6, 3) ? '' : 'images/';
-
-$i = 1;
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_HOME;
-$adminmenu[$i]['link'] = 'admin/index.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ADMIN_HOME_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/home.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADADM_ADMENU1;
-$adminmenu[$i]['link'] = 'admin/main.php';
-$adminmenu[$i]['desc'] = _MI_TADADM_ADMENU1;
-$adminmenu[$i]['icon'] = 'images/admin/update.png';
-$i++;
-$adminmenu[$i]['title'] = _MI_TADADM_XOOPS_UPGRADE;
-$adminmenu[$i]['link'] = 'admin/xoops.php';
-$adminmenu[$i]['desc'] = _MI_TADADM_XOOPS_UPGRADE;
-$adminmenu[$i]['icon'] = 'images/admin/upload.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADADM_ADMENU3;
-$adminmenu[$i]['link'] = 'admin/spam.php';
-$adminmenu[$i]['desc'] = _MI_TADADM_ADMENU3;
-$adminmenu[$i]['icon'] = 'images/admin/spam.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADADM_ADMENU2;
-$adminmenu[$i]['link'] = 'admin/phpini.php';
-$adminmenu[$i]['desc'] = _MI_TADADM_ADMENU2;
-$adminmenu[$i]['icon'] = 'images/admin/php.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADADM_ADMENU4;
-$adminmenu[$i]['link'] = 'admin/backup.php';
-$adminmenu[$i]['desc'] = _MI_TADADM_ADMENU4;
-$adminmenu[$i]['icon'] = 'images/admin/backup.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADADM_ADMENU5;
-$adminmenu[$i]['link'] = 'admin/clean.php';
-$adminmenu[$i]['desc'] = _MI_TADADM_ADMENU5;
-$adminmenu[$i]['icon'] = 'images/admin/clean.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_ABOUT;
-$adminmenu[$i]['link'] = 'admin/about.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ADMIN_ABOUT_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/about.png';
+$adminmenu = [
+    ['title' => _MI_TADADM_ADMENU1, 'link' => 'admin/main.php', 'icon' => 'images/admin/update.png'],
+    ['title' => _MI_TADADM_XOOPS_UPGRADE, 'link' => 'admin/xoops.php', 'icon' => 'images/admin/upload.png'],
+    ['title' => _MI_TADADM_ADMENU3, 'link' => 'admin/spam.php', 'icon' => 'images/admin/spam.png'],
+    ['title' => _MI_TADADM_ADMENU2, 'link' => 'admin/phpini.php', 'icon' => 'images/admin/php.png'],
+    ['title' => _MI_TADADM_ADMENU4, 'link' => 'admin/backup.php', 'icon' => 'images/admin/backup.png'],
+    ['title' => _MI_TADADM_ADMENU5, 'link' => 'admin/clean.php', 'icon' => 'images/admin/clean.png'],
+    ['title' => _MI_TAD_ADMIN_ABOUT, 'link' => 'admin/about.php', 'icon' => 'images/admin/about.png'],
+];
