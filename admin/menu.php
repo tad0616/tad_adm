ac2a16318e2769964c652372fd847bf9481f805e
tad0616 <tad0616@gmail.com>
2025-10-30 16:37:33 +0800

    修正 Adminer readable-dates 外掛顯示錯誤問題

--- admin/menu.php
+++ admin/menu.php
@@ -2,9 +2,9 @@
 $adminmenu = [
     ['title' => _MI_TADADM_ADMENU1, 'link' => 'admin/main.php', 'icon' => 'images/admin/update.png'],
     ['title' => _MI_TADADM_XOOPS_UPGRADE, 'link' => 'admin/xoops.php', 'icon' => 'images/admin/upload.png'],
-    ['title' => _MI_TADADM_ADMENU3, 'link' => 'admin/spam.php', 'icon' => 'images/admin/spam.png'],
     ['title' => _MI_TADADM_ADMENU2, 'link' => 'admin/phpini.php', 'icon' => 'images/admin/php.png'],
-    ['title' => _MI_TADADM_ADMENU4, 'link' => 'admin/backup.php', 'icon' => 'images/admin/backup.png'],
     ['title' => _MI_TADADM_ADMENU5, 'link' => 'admin/clean.php', 'icon' => 'images/admin/clean.png'],
+    ['title' => _MI_TADADM_ADMENU3, 'link' => 'admin/spam.php', 'icon' => 'images/admin/spam.png'],
+    ['title' => _MI_TADADM_ADMENU4, 'link' => 'admin/backup.php', 'icon' => 'images/admin/backup.png'],
     ['title' => _MI_TAD_ADMIN_ABOUT, 'link' => 'admin/about.php', 'icon' => 'images/admin/about.png'],
 ];
--- adminer-plugins/readable-dates.php
+++ adminer-plugins/readable-dates.php
@@ -1,49 +1,68 @@
 <?php
 
 /** This plugin replaces UNIX timestamps with human-readable dates in your local format.
-* Mouse click on the date field reveals timestamp back.
-*
-* @link https://www.adminer.org/plugins/#use
-* @author Anonymous
-* @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
-* @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
-*/
-class AdminerReadableDates {
-	/** @access protected */
-	var $prepend;
+ * Mouse click on the date field reveals timestamp back.
+ *
+ * @link https://www.adminer.org/plugins/#use
+ * @author Anonymous
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
+ */
+class AdminerReadableDates
+{
+    /** @access protected */
+    public $prepend;
 
-	function __construct() {
-		$this->prepend = <<<EOT
+    public function __construct()
+    {
+        $this->prepend = <<<EOT
 
 document.addEventListener('DOMContentLoaded', function(event) {
 	var date = new Date();
 	var tds = document.querySelectorAll('td[id^="val"]');
+
+	// UNIX timestamp 的合理範圍 (1970-2038年)
+	var minTimestamp = 0;          // 1970-01-01
+	var maxTimestamp = 2147483647; // 2038-01-19 (32位系統最大值)
+
 	for (var i = 0; i < tds.length; i++) {
 		var text = tds[i].innerHTML.trim();
+
+		// 檢查是否為10位數字
 		if (text.match(/^\d{10}$/)) {
-			date.setTime(parseInt(text) * 1000);
-			tds[i].oldDate = text;
+			var timestamp = parseInt(text);
 
-			// tds[i].newDate = date.toUTCString().substr(5); // UTC format
-			tds[i].newDate = date.toLocaleString();	// Local format
-			// tds[i].newDate = date.toLocaleFormat('%e %b %Y %H:%M:%S'); // Custom format - works in Firefox only
+			// 檢查是否在合理的時間戳範圍內
+			// 排除以09開頭的數字（台灣手機號碼常見格式）
+			if (timestamp >= minTimestamp &&
+			    timestamp <= maxTimestamp &&
+			    !text.match(/^09/)) {
 
-			tds[i].newDate = '<span style="color: #009900">' + tds[i].newDate + '</span>';
-			tds[i].innerHTML = tds[i].newDate;
-			tds[i].dateIsNew = true;
+				date.setTime(timestamp * 1000);
+				tds[i].oldDate = text;
 
-			tds[i].addEventListener('click', function(event) {
-				this.innerHTML = (this.dateIsNew ? this.oldDate : this.newDate);
-				this.dateIsNew = !this.dateIsNew;
-			});
+				// tds[i].newDate = date.toUTCString().substr(5); // UTC format
+				tds[i].newDate = date.toLocaleString();	// Local format
+				// tds[i].newDate = date.toLocaleFormat('%e %b %Y %H:%M:%S'); // Custom format - works in Firefox only
+
+				tds[i].newDate = '<span style="font-size:11px;color: #009900" title="' + tds[i].oldDate + '">' + tds[i].newDate + '</span>';
+				tds[i].innerHTML = tds[i].newDate;
+				tds[i].dateIsNew = true;
+
+				tds[i].addEventListener('click', function(event) {
+					this.innerHTML = (this.dateIsNew ? this.oldDate : this.newDate);
+					this.dateIsNew = !this.dateIsNew;
+				});
+			}
 		}
 	}
 });
 
 EOT;
-	}
+    }
 
-	function head() {
-		echo Adminer\script($this->prepend);
-	}
+    public function head()
+    {
+        echo Adminer\script($this->prepend);
+    }
 }
